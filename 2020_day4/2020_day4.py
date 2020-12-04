1a11eb476c150cd0903b338bc9c7bdd81385d0e5
LJFHendriks <leo.hendriks@live.nl>
2020-12-04 14:57:15 +0100

    Solve Advent of Code 2020 day 4

--- 2020_day4/2020_day4.py
+++ 2020_day4/2020_day4.py
@@ -1,26 +1,110 @@
-import numpy as np
+from functools import partial
 
-def day3part1(right,down):
+def day4part1():
     f = open("input.txt")
 
     str = f.read()
 
 
-    arr = str.split()
+    arr = str.split("\n\n")
 
-    width = len(arr[0])
+    validpass = 0;
+    for i in range(len(arr)):
+        if allData(arr[i]):
+            validpass += 1
+    return validpass
 
-    trees = 0
-    pos = 0
-    for i in range(0,len(arr),down):
-        if arr[i][pos % width] == "#":
-            trees += 1
-        pos += right
-    return trees
+def allData(pas):
+    if "byr:" in pas:
+        if "iyr:" in pas:
+            if "eyr:" in pas:
+                if "hgt:" in pas:
+                    if "hcl:" in pas:
+                        if "ecl:" in pas:
+                            if "pid:" in pas:
+                                return True
+    return False
+
 
 def day3part2():
+    f = open("input.txt")
 
-    return day3part1(1,1) * day3part1(3,1) * day3part1(5,1) * day3part1(7,1) * day3part1(1,2)
+    str = f.read()
 
-print(f"The number of trees hit in task 1 equals: {day3part1(3,1)}")
-print(f"The multiple of the number of trees hit equals: {day3part2()}")
+    arr = str.split("\n\n")
+
+    validpass = 0;
+    for i in range(len(arr)):
+        if isValidPass(arr[i]):
+            validpass += 1
+    return validpass
+
+def year(lower,upper,value):
+    if len(value) == 4:
+        value = int(value)
+        if value >= lower:
+            if value <= upper:
+                return True
+    return False
+
+def hgt(value):
+    if value[-2:] == "cm":
+        value = int(value[:-2])
+        if value >= 150:
+            if value <= 193:
+                return True
+    elif value[-2:] == "in":
+        value = int(value[:-2])
+        if value >= 59:
+            if value <= 76:
+                return True
+    return False
+
+def hcl(value):
+    if value[0] == "#":
+        if len(value[1:]) == 6:
+            for i in range(1,7):
+                if not value[i] in "0123456789abcdef":
+                    return False
+            return True
+    return False
+
+def ecl(value):
+    if value in ["amb", "blu", "brn", "gry", "grn", "hzl", "oth"]:
+        return True
+    return False
+
+def pid(value):
+    if len(value) == 9:
+        if value.isdigit():
+            return True
+    return False
+
+def error(pas):
+    raise Exception("Unknown identifier.")
+
+def isValidPass(pas):
+    if allData(pas):
+        pas = pas.split()
+        switcher = {
+            "byr:": partial(year,1920,2002),
+            "iyr:": partial(year,2010,2020),
+            "eyr:": partial(year,2020,2030),
+            "hgt:": hgt,
+            "hcl:": hcl,
+            "ecl:": ecl,
+            "pid:": pid,
+            "cid:": lambda pas: True
+        }
+        for i in range(len(pas)):
+            func = switcher.get(pas[i][:4],error)
+            if not func(pas[i][4:]):
+                return False
+        return True
+    return False
+
+
+
+
+print(f"The number of valid passwords in task 1 equals: {day4part1()}")
+print(f"The number of valid passwords in task 2 equals: {day3part2()}")
